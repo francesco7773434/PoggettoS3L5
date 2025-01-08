14999bd1001de3efce59445d87d56b394e09d0e9
francesco7773434 <francescoorbello5@gmail.com>
2025-01-08 08:58:55 +0100

    compito corretto

--- assets/D10.js
+++ assets/D10.js
@@ -13,29 +13,54 @@ REGOLE
   Crea una variabile chiamata "sum" e assegnaci il risultato della somma tra i valori 10 e 20.
 */
 
+let sum = 10 + 20;
+console.log(sum);
+
 /* ESERCIZIO B
   Crea una variabile chiamata "random" e assegnaci un numero casuale tra 0 e 20 (deve essere generato dinamicamente a ogni esecuzione).
 */
+
+let random = Math.floor(Math.random() * 21);
+console.log(random);
 
 /* ESERCIZIO C
   Crea una variabile chiamata "me" e assegnaci un oggetto contenente le seguenti proprietà: name = il tuo nome, surname = il tuo cognome, age = la tua età.
 */
 
+let me = {
+  name: "Frank",
+  surname: "Orbello",
+  age: 24,
+}
+console.log(me);
+
 /* ESERCIZIO D
   Crea del codice per rimuovere programmaticamente la proprietà "age" dall'oggetto precedentemente creato.
 */
+delete me.age;
+console.log(me);
+
 
 /* ESERCIZIO E
   Crea del codice per aggiungere programmaticamente all'oggetto precedentemente creato un array chiamato "skills", contenente i linguaggi di programmazione che conosci.
 */
 
+me.skills = ["javaScript", "phyton"];
+console.log(me)
+
 /* ESERCIZIO F
   Crea un pezzo di codice per aggiungere un nuovo elemento all'array "skills" contenuto nell'oggetto "me".
 */
 
+me.skills.push("angular");
+console.log(me)
+
 /* ESERCIZIO G
   Crea un pezzo di codice per rimuovere programmaticamente l'ultimo elemento dall'array "skills" contenuto nell'oggetto "me".
 */
+
+me.skills.splice(2);
+console.log(me);
 
 // Funzioni
 
@@ -43,9 +68,27 @@ REGOLE
   Crea una funzione chiamata "dice": deve generare un numero casuale tra 1 e 6.
 */
 
+function dice(){
+  let num = Math.floor(Math.random() * 6) +1;
+  return num;
+}
+console.log(dice())
+
 /* ESERCIZIO 2
   Crea una funzione chiamata "whoIsBigger" che riceve due numeri come parametri e ritorna il maggiore dei due.
 */
+
+
+function whoIsBigger(num1 , num2){
+  if(num1 > num2){
+    return num1;
+  }
+  else{
+    return num2;
+  }
+}
+
+console.log(whoIsBigger(30,50));
 
 /* ESERCIZIO 3
   Crea una funzione chiamata "splitMe" che riceve una stringa come parametro e ritorna un'array contenente ogni parola della stringa.
@@ -53,10 +96,30 @@ REGOLE
   Es.: splitMe("I love coding") => ritorna ["I", "Love", "Coding"]
 */
 
+function splitMe(string){
+  return string.split(" ");
+}
+
+console.log(splitMe("Io sono Frank"));
+
+
+
 /* ESERCIZIO 4
   Crea una funzione chiamata "deleteOne" che riceve una stringa e un booleano come parametri.
   Se il valore booleano è true la funzione deve ritornare la stringa senza il primo carattere, altrimenti la deve ritornare senza l'ultimo.
 */
+
+function deletOne(str, remove){
+  if(remove){
+   return str.slice(1);
+  }
+  else{
+   return str.slice(0, -1);
+  }
+}
+console.log(deletOne("Frank", true));
+console.log(deletOne("Frank", false));
+
 
 /* ESERCIZIO 5
   Crea una funzione chiamata "onlyLetters" che riceve una stringa come parametro e la ritorna eliminando tutte le cifre numeriche.
@@ -64,13 +127,38 @@ REGOLE
   Es.: onlyLetters("I have 4 dogs") => ritorna "I have dogs"
 */
 
+function onlyLetters(stringa){
+  return stringa.replace(/\d/g, "");
+}
+console.log(onlyLetters("Io ho 2 cani"));
+
+
+
 /* ESERCIZIO 6
   Crea una funzione chiamata "isThisAnEmail" che riceve una stringa come parametro e ritorna true se la stringa è un valido indirizzo email.
 */
 
+function isThisAnEmail(email){
+  let regex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
+console.log(isThisAnEmail("francesco@gmail.com"))
+
+
 /* ESERCIZIO 7
   Scrivi una funzione chiamata "whatDayIsIt" che ritorna il giorno della settimana corrente.
 */
+
+
+function whatDayIsIt(){
+  let daysOfWeek = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"]
+   let nowDay = new Date();
+   let dayIndex = nowDay.getDate();
+   return daysOfWeek[dayIndex]
+}
+console.log(whatDayIsIt());
+
+
 
 /* ESERCIZIO 8
   Scrivi una funzione chiamata "rollTheDices" che riceve un numero come parametro.
@@ -85,13 +173,58 @@ REGOLE
   }
 */
 
+function rollTheDices(number2){
+  let values = [];
+  let sum = 0;
+
+  for(let i = 0; i < number2; i++){
+    let roll = dice();
+    values.push(roll);
+    sum += roll;
+  }
+  return{sum, values};
+}
+console.log(rollTheDices(5));
+
+
+
 /* ESERCIZIO 9
   Scrivi una funzione chiamata "howManyDays" che riceve una data come parametro e ritorna il numero di giorni trascorsi da tale data.
 */
 
+function howmanyDays(date){
+  let currentDate = new Date();
+  let pastDate = new Date(date);
+  let differenceTime = currentDate - pastDate;
+  
+  let differenceDays = Math.floor(differenceTime / (1000 * 3600 *24));
+  return differenceDays;
+  
+ }
+ console.log(howmanyDays("2024-12-12"));
+
 /* ESERCIZIO 10
   Scrivi una funzione chiamata "isTodayMyBirthday" che deve ritornare true se oggi è il tuo compleanno, falso negli altri casi.
 */
+
+function isTodayMyBirthday(myDate){
+  let dateCurrent = new Date();
+  let birthdayDate = new Date(myDate);
+  
+  if(dateCurrent.getDate() === birthdayDate.getDate() &&
+     dateCurrent.getMonth()=== birthdayDate.getMonth() &&
+      dateCurrent.getFullYear() === birthdayDate.getFullYear()){
+    return true;
+  }
+  else{
+    return false
+  }
+  
+
+}
+
+console.log(isTodayMyBirthday("2024-03-06"));
+
 
 // Arrays & Oggetti
 
@@ -101,6 +234,20 @@ REGOLE
   Scrivi una funzione chiamata "deleteProp" che riceve un oggetto e una stringa come parametri; deve ritornare l'oggetto fornito dopo aver eliminato
   in esso la proprietà chiamata come la stringa passata come secondo parametro.
 */
+
+function deleteProp(obj, str){
+  delete obj[str];
+  return obj
+   
+}
+let obj ={
+  name: "Frank",
+  city: "NewYork"
+ };
+ 
+console.log(deleteProp(obj, "city" ))
+
+
 
 /* ESERCIZIO 12
   Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
@@ -341,19 +488,39 @@ textTd()
 /* ESERCIZIO 23
   Scrivi una funzione per aggiungere un background di colore rosso a ogni link all'interno della pagina.
 */
-
+function colorBackground(){
+  let totalLinks = document.querySelectorAll("link");
+  totalLinks.style.backgrounColor = "red"
+}
+colorBackground()
 /* ESERCIZIO 24
   Scrivi una funzione per aggiungere un nuovo elemento alla lista non ordinata con id "myList".
 */
+function newArray(){
+  let myArr = document.querySelector("#myList");
+  let newMyArr = document.createElement("li");
+  myArr.appendChild(newMyArr);
+}
+
+newArray()
 
 /* ESERCIZIO 25
   Scrivi una funzione per svuotare la lista non ordinata con id "myList".
 */
-
+function removeList(){
+ let myList = document.querySelector("#myList");
+ let elementList = document.querySelector("li");
+ myList.removeChild(elementList)
+}
+removeList()
 /* ESERCIZIO 26
   Scrivi una funzione per aggiungere ad ogni tag <tr> la classe CSS "test"
 */
-
+function classTest(){
+   let trTest = querySelectorAll("tr");
+   trTest.classList.add("test")
+}
+classTest()
 // [EXTRA] JS Avanzato
 
 /* ESERCIZIO 27
